0f5738f6b4a69e995a86e68e206d092628c2f480
Omkar Deepak Mane <117914964+maneom@users.noreply.github.com>
2024-04-15 17:12:57 +0530

    Create butterFly.py

---
+++ butterFly.py
@@ -0,0 +1,21 @@
+n = 5
+
+# Upper half of the butterfly
+for i in range(1, n + 1):
+    for j in range(i):
+        print("*", end="")
+    for j in range(2 * (n - i)):
+        print(" ", end="")
+    for j in range(i):
+        print("*", end="")
+    print()
+
+# Lower half of the butterfly
+for i in range(n, 0, -1):
+    for j in range(i):
+        print("*", end="")
+    for j in range(2 * (n - i)):
+        print(" ", end="")
+    for j in range(i):
+        print("*", end="")
+    print()
